14b3d477d50b014725649891f635ad211d3540af
HRida <H.Rida1@hotmail.com>
2020-05-23 11:08:42 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi I am the client...");
+
+        //  IPAddress[] ipAddress = Dns.GetHostAddresses("localhost");
+        //  IPEndPoint ipEnd = new IPEndPoint(ipAddress[1], 5656);      //127.0.0.1
+
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(address, 5656); 
+            Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSock.Connect(ipEnd);
+
+            string strData = "Message from client end.";
+            byte[] clientData = new byte[1024];
+            clientData = Encoding.ASCII.GetBytes(strData);
+            clientSock.Send(clientData);
+
+            byte[] serverData = new byte[1024];
+            int len = clientSock.Receive(serverData);
+            Console.WriteLine(Encoding.ASCII.GetString(serverData, 0, len));
+
+            clientSock.Close();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi I am the server...");
+        //  IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 5656);
+
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(address, 5656);
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(ipEnd);
+            sock.Listen(100);
+
+            Socket clientSock = sock.Accept();
+            byte[] clientData = new byte[1024];
+            int receivedBytesLen = clientSock.Receive(clientData);
+            string clientDataInString = Encoding.ASCII.GetString(clientData, 0, receivedBytesLen);
+            Console.WriteLine("Received Data {0}", clientDataInString);
+
+            string clientStr = "Client Data Received";
+            byte[] sendData = new byte[1024];
+            sendData = Encoding.ASCII.GetBytes(clientStr);
+            clientSock.Send(sendData);
+
+            clientSock.Close();
+            Console.ReadLine();
+        }
+    }
+}
